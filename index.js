b275db9a62790632a9eaf3f6a068fb958143174e
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-11-09 15:35:34 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const TGA = require('tga');
+const PNG = require('pngjs').PNG;
+
+module.exports = function(source) {
+	const callback = this.async();
+
+	const tga = new TGA(source);
+	const png = new PNG({
+		width: tga.width,
+		height: tga.height
+	});
+	png.data = Buffer.from(tga.pixels);
+
+	const stream = png.pack();
+	const buffers = [];
+	stream.on('data', data => buffers.push(data));
+	stream.on('end', () => callback(null, Buffer.concat(buffers)));
+};
+
+module.exports.raw = true;
